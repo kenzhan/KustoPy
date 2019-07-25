f3c43fe2b161953380514a3cb49a3001ce3f8d79
ken <kenneth_zh@msn.com>
2019-07-24 22:58:27 -0700

    add requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
     version='0.1',
     url='https://github.com/kenzhan/KustoPy',
     author='Ken',
-    author_email='kenneth_zh@msn.com',
     keywords=['pip','Kusto','Azure Data Explorer']
     )
